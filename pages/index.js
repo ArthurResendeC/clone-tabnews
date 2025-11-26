934ccc5e46f0564d9b9b5f373700d753b251c0b3
Arthur Resende <156852688+ArthurResendeC@users.noreply.github.com>
2025-11-26 01:14:36 +0000

    feat: add a `welcome message` to home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-    return <h1>Página inicial</h1>
+    return (
+        <>
+            <h1>Ei! Você desbloqueou a fase “em construção”.</h1>
+            <p>Ainda estou em processo para virar algo ainda melhor. Faz o seguinte, passa aqui depois! <i>(E valeu por aparecer antes mesmo do lançamento oficial!)</i> 😄</p>
+        </>
+    )
 }
