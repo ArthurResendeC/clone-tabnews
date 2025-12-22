e5692dec694e2b94ef2608590b514a003fa4b040
Arthur Resende <arthur.resendecoura@hotmail.com>
2025-12-22 00:31:42 -0300

    refactor: streamline method handling and enhance error management in `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,38 +3,48 @@ import migrationRunner from "node-pg-migrate";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    client: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dir: resolve("infra", "migrations"),
-    dryRun: true,
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  if (request.method === "GET") {
-    console.log("Entrou no GET");
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
     });
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
   }
 
-  if (request.method === "POST") {
-    console.log("Entrou no POST");
-    const appliedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    if (appliedMigrations.length > 0) {
-      return response.status(201).json(appliedMigrations);
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      client: dbClient,
+      databaseUrl: process.env.DATABASE_URL,
+      dir: resolve("infra", "migrations"),
+      dryRun: true,
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+      });
+      return response.status(200).json(pendingMigrations);
     }
-    return response.status(200).json(appliedMigrations);
+
+    if (request.method === "POST") {
+      const appliedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+      if (appliedMigrations.length > 0) {
+        return response.status(201).json(appliedMigrations);
+      }
+      return response.status(200).json(appliedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
-  console.log("Passou direto");
-  await dbClient.end();
-  return response.status(405).json({ message: "Method not allowed" });
 }
